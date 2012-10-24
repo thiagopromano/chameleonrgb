ddd4da27b554f1475ebca3ddde51fece857bbcf3
thiagopromano <Telecom01@Telecom01-HP>
2012-10-24 17:52:18 -0200

    Refeito o menu

--- src/br/edu/ifpr/ThiagoRomano/Camaleao/MainMenuScene.java
+++ src/br/edu/ifpr/ThiagoRomano/Camaleao/MainMenuScene.java
@@ -1,38 +1,88 @@
 package br.edu.ifpr.ThiagoRomano.Camaleao;
 
-import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.scene.menu.MenuScene;
 import org.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
 import org.andengine.entity.scene.menu.item.IMenuItem;
 import org.andengine.entity.scene.menu.item.SpriteMenuItem;
+import org.andengine.entity.sprite.Sprite;
 
 public class MainMenuScene extends MenuScene implements
 		IOnMenuItemClickListener {
 	CamaleaotestActivity activity;
+
+	Sprite mBackground;
+	Sprite mChameleon;
+	Sprite mTabua;
+	Sprite mTitle;
+
+	IMenuItem mNinjaModeButton;
+	IMenuItem mArcadeModeButton;
+	IMenuItem mAbout;
+	IMenuItem mCredits;
+
 	final int MENU_ARCADE = 0;
-	final int MENU_TIMEATTACK = 1;
+	final int MENU_NINJA = 1;
+	final int MENU_ABOUT = 2;
+	final int MENU_CREDITS = 3;
 
 	public MainMenuScene() {
 
 		activity = CamaleaotestActivity.getSharedInstance();
 		this.mCamera = activity.mCamera;
-		setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
-		IMenuItem timeAttackButton = new SpriteMenuItem(MENU_TIMEATTACK, 
+
+		mNinjaModeButton = new SpriteMenuItem(MENU_NINJA,
 				activity.mSpritesheetTexturePackTextureRegionLibrary
-				.get(posicoes.TIMEATTACKBUTTON_ID),
+						.get(posicoes.NINJA_ID),
 				activity.getVertexBufferObjectManager());
-		timeAttackButton.setPosition(100, 320);
-		
-		IMenuItem ArcadeButton = new SpriteMenuItem(MENU_ARCADE, 
+		mNinjaModeButton.setPosition(96, 472);
+
+		mArcadeModeButton = new SpriteMenuItem(MENU_ARCADE,
 				activity.mSpritesheetTexturePackTextureRegionLibrary
-				.get(posicoes.ARCADEBUTTON_ID),
+						.get(posicoes.ARCADE_ID),
 				activity.getVertexBufferObjectManager());
-		ArcadeButton.setPosition(300, 320);
+		mArcadeModeButton.setPosition(140, 380);
 		
+		mAbout = new SpriteMenuItem(MENU_ABOUT,
+				activity.mSpritesheetTexturePackTextureRegionLibrary
+				.get(posicoes.ABOUT_ID),
+				activity.getVertexBufferObjectManager());
+		mAbout.setPosition(165, 567);
 		
-		addMenuItem(timeAttackButton);
-		addMenuItem(ArcadeButton);
+		mCredits = new SpriteMenuItem(MENU_CREDITS,
+				activity.mSpritesheetTexturePackTextureRegionLibrary
+				.get(posicoes.CREDITS_ID),
+				activity.getVertexBufferObjectManager());
+		mCredits.setPosition(137, 659);
+
+		mBackground = new Sprite(0, 0,
+				activity.mSpritesheetTexturePackTextureRegionLibrary
+						.get(posicoes.MENU_BG_ID),
+				activity.getVertexBufferObjectManager());
 		
+		mChameleon = new Sprite(277, 256,
+				activity.mSpritesheetTexturePackTextureRegionLibrary
+				.get(posicoes.CHAMELEON_ID),
+				activity.getVertexBufferObjectManager());
+		
+		mTabua = new Sprite(64, 350,
+				activity.mSpritesheetTexturePackTextureRegionLibrary
+				.get(posicoes.TABUA_ID),
+				activity.getVertexBufferObjectManager());
+		
+		mTitle = new Sprite(28, 36,
+				activity.mSpritesheetTexturePackTextureRegionLibrary
+				.get(posicoes.TITLE_ID),
+				activity.getVertexBufferObjectManager());
+		
+		attachChild(mBackground);
+		attachChild(mTabua);
+		addMenuItem(mCredits);
+		addMenuItem(mNinjaModeButton);
+		addMenuItem(mArcadeModeButton); 
+		addMenuItem(mAbout);
+		attachChild(mTitle);
+		attachChild(mChameleon);
+
 		setBackgroundEnabled(false);
 		setOnMenuItemClickListener(this);
 
@@ -42,20 +92,18 @@ public class MainMenuScene extends MenuScene implements
 	public boolean onMenuItemClicked(MenuScene arg0, IMenuItem arg1,
 			float arg2, float arg3) {
 		switch (arg1.getID()) {
-		case MENU_TIMEATTACK:
+		case MENU_NINJA:
 			activity.setCurrentScene(new NinjaModeScene());
 			return true;
-		case MENU_ARCADE:
-		{
+		case MENU_ARCADE: {
 			activity.setCurrentScene(new ArcadeModeScene());
 			return true;
 		}
 		default:
 			break;
-		
+
 		}
 		return false;
 	}
-	
 
 }
--- src/br/edu/ifpr/ThiagoRomano/Camaleao/posicoes.java
+++ src/br/edu/ifpr/ThiagoRomano/Camaleao/posicoes.java
@@ -1,7 +1,7 @@
 // Definitions for sprite sheet positions
 // Created with www.texturepacker.com
 
-// $TexturePacker:SmartUpdate:fe93d4e2f57a3014aa60b16a4323fa2b$
+// $TexturePacker:SmartUpdate:9daba31f127a32f2874c058b4df77e42$
 
 package br.edu.ifpr.ThiagoRomano.Camaleao;
 
@@ -12,15 +12,23 @@ public interface posicoes
 	public static final int CAMALEAO_MASK_TROCACOR_ID = 2;
 	public static final int CAMALEAO_SOMBRA_ID = 3;
 	public static final int G_ID = 4;
-	public static final int NYLON_DETALHES_ID = 5;
-	public static final int NYLON_TROCACOR_ID = 6;
-	public static final int PALCO_ID = 7;
-	public static final int R_ID = 8;
-	public static final int TIMEATTACKBUTTON_ID = 9;
-	public static final int BOX_ID = 10;
-	public static final int CHRONOMETROFUNDO_ID = 11;
-	public static final int FOLHAS_FRENTE_ID = 12;
-	public static final int FUNDO_ID = 13;
-	public static final int MARCADOR_ID = 14;
-	public static final int TRONCO_ID = 15;
+	public static final int MENU_BG_ID = 5;
+	public static final int NYLON_DETALHES_ID = 6;
+	public static final int NYLON_TROCACOR_ID = 7;
+	public static final int PALCO_ID = 8;
+	public static final int R_ID = 9;
+	public static final int TIMEATTACKBUTTON_ID = 10;
+	public static final int ABOUT_ID = 11;
+	public static final int ARCADE_ID = 12;
+	public static final int BOX_ID = 13;
+	public static final int CHAMELEON_ID = 14;
+	public static final int CHRONOMETROFUNDO_ID = 15;
+	public static final int CREDITS_ID = 16;
+	public static final int FOLHAS_FRENTE_ID = 17;
+	public static final int FUNDO_ID = 18;
+	public static final int MARCADOR_ID = 19;
+	public static final int NINJA_ID = 20;
+	public static final int TABUA_ID = 21;
+	public static final int TITLE_ID = 22;
+	public static final int TRONCO_ID = 23;
 }
